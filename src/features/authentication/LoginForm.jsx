679237a14722afd861c8e2596f322ba5abc210bb
Felix Felix <felixharris11@gmail.com>
2025-08-24 14:47:39 +0700

    clearing credentials

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("felix@example.com");
-  const [password, setPassword] = useState("p@ssw0rd");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
